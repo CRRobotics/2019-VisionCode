44104d4fbaa35a8cff8fb7a4d5b8d8454364eaf1
aidenBedore <46579405+aidenBedore@users.noreply.github.com>
2019-01-11 20:49:13 -0500

    vcxz
    vcxz

--- aiden/fg.py
+++ aiden/fg.py
@@ -3,21 +3,21 @@ import numpy as np
 from grip import GripPipeline
 import argparse
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 pipeline = GripPipeline()
 while True:
     # Capture frame-by-frame
     ret, frame_in = cap.read()
     pipeline.process(frame_in)
-    #print(frame_in)
+    # print(frame_in)
     frame = np.zeros(frame_in.shape[:-1], frame_in.dtype)
-    #print(frame)
-    #cv2.drawContours(frame, pipeline.find_contours_output, -1, 255, 2)
+    # print(frame)
+    # cv2.drawContours(frame, pipeline.find_contours_output, -1, 255, 2)
     cv2.fillPoly(frame, pts=pipeline.find_contours_output, color=255)
 
-    #frame = frame_in
+    # frame = frame_in
     # Our operations on the frame come here
-    circles = cv2.HoughCircles(frame, cv2.HOUGH_GRADIENT, 1.5, 100)
+    circles = cv2.HoughCircles(frame, cv2.HOUGH_GRADIENT, 1.6, 100)
 
     # ensure at least some circles were found
     if circles is not None:
@@ -28,8 +28,8 @@ while True:
         for (x, y, r) in circles:
             # draw the circle in the output image, then draw a rectangle
             # corresponding to the center of the circle
-            cv2.circle(frame_in, (x, y), r, (200, 15, 15), 4)
-            cv2.rectangle(frame_in, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+            cv2.circle(frame, (x, y), r, (200, 15, 15), 4)
+            cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
 
     # Display the resulting frame
     if cv2.waitKey(1) & 0xFF == ord('q'):
